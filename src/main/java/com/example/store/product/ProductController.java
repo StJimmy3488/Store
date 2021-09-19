0bc513ccedbaeba307714083d640aaca6d7550fe
Optimus2525 <ilmrsm@gmail.com>
2021-09-19 20:32:01 +0300

    Addes settings to application.properties, Start page and Products page, Thymeleaf "layours" library

--- src/main/java/com/example/store/product/ProductController.java
+++ src/main/java/com/example/store/product/ProductController.java
@@ -1,8 +1,21 @@
 package com.example.store.product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/products")
+
 public class ProductController {
+
+    @GetMapping("/all_products")
+    public String getAllProducts() {
+
+        return "/products/all_products";
+    }
+
+
 
 }
