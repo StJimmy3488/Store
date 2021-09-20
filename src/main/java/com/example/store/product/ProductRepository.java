26cd20f0acdb038c23a1a8d192db595c486235b2
Janis <jan.dreimanis@gmail.com>
2021-09-20 22:16:39 +0300

    Added one method to ProductRepository Added more methods to ProductService Added more testMethods to productServiceTest

--- src/main/java/com/example/store/product/ProductRepository.java
+++ src/main/java/com/example/store/product/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -11,7 +12,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     // This is adding pagination functionality
     Page<Product> findAll(Pageable pageable);
 
-
-
+    Optional<Product> findProductByProductName(String name);
 
 }
--- src/main/java/com/example/store/product/ProductService.java
+++ src/main/java/com/example/store/product/ProductService.java
@@ -4,16 +4,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
-    ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
-    public Product saveProduct(Product product) {
+    public Product saveOrUpdateProduct(Product product) {
         product.setProductCreatedAt(LocalDate.now());
         return productRepository.save(product);
+    }
+
+    public List<Product> findAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findByProductName(String name) {
+        return productRepository.findProductByProductName(name);
+    }
+
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
     }
 
 
