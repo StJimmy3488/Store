be9cdb550aa20abf8d1b7d36060c58fd0670f44e
Optimus2525 <ilmrsm@gmail.com>
2021-09-21 16:01:52 +0300

    Implemented reading products from DB and showing in products page

--- src/main/java/com/example/store/product/ProductService.java
+++ src/main/java/com/example/store/product/ProductService.java
@@ -1,13 +1,20 @@
 package com.example.store.product;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ProductService {
 
