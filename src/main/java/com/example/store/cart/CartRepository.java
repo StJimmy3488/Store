feadb27b4d690017aa5aea6d08143e1ddd53988d
Linda <austelinda@gmail.com>
2021-09-26 14:09:52 +0300

    Created CartService Layer

--- src/main/java/com/example/store/cart/CartRepository.java
+++ src/main/java/com/example/store/cart/CartRepository.java
@@ -1,6 +1,13 @@
 package com.example.store.cart;
 
+import com.example.store.cart.Cart;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Page<Cart> findAll(Pageable pageable);
+
+    Cart findById();
 }
