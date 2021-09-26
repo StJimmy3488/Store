c30bfdd8e3bac3d7acfeed506dd7a1adaffcb98a
Linda <austelinda@gmail.com>
2021-09-26 14:29:40 +0300

    Created CartItemService Layer

--- src/main/java/com/example/store/cart_item/CartItemService.java
+++ src/main/java/com/example/store/cart_item/CartItemService.java
@@ -1,4 +1,26 @@
 package com.example.store.cart_item;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
 public class CartItemService {
+
+    private final CartItemRepository cartItemRepository;
+
+    public List<CartItem> getAllCartItems(){
+        return cartItemRepository.findAll();
+    }
+
+    public CartItem getById(Long cartItemId){
+        return cartItemRepository.getById(cartItemId);
+    }
+
+    public void deleteById(Long cartItemId){
+        cartItemRepository.deleteById(cartItemId);
+    }
 }
