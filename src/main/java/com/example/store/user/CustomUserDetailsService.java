5e380ea600f83c37def8d88e4f55d0f987ce7834
Optimus2525 <ilmrsm@gmail.com>
2021-09-28 14:36:34 +0300

    Updated CustomUserDetailsService class

--- src/main/java/com/example/store/user/CustomUserDetailsService.java
+++ src/main/java/com/example/store/user/CustomUserDetailsService.java
@@ -1,64 +1,48 @@
 package com.example.store.user;
 
-import com.example.store.role.Role;
-import com.example.store.role.RoleRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Set;
-
-
-import static java.util.stream.Collectors.toList;
-
-
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
+//    private final RoleRepository roleRepository;
+//    private final PasswordEncoder passwordEncoder;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = userRepository.findByUsernameIgnoreCase(username)
-                .orElseThrow(() -> new RuntimeException(username + " not found"));
-
-        var authorities = mapRolesToAuthorities(user.getRoles());
-
-        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getUserPassword(), authorities);
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameIgnoreCase(userName)
+                .orElseThrow(() -> new RuntimeException(userName + " could not be found"));
+        return new MyUserDetails(user);
+//        var authorities = mapRolesToAuthorities(user.getRoles());
+//        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getUserPassword(), authorities);
     }
 
-    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
-        return roles.stream()
-                .map(role -> new SimpleGrantedAuthority(role.getRoleName()))
-                .collect(toList());
-    }
-
-    public User registerUser(User user) {
-        boolean isUserExisting = userRepository.existsByUsername(user.getUserName());
-        if (isUserExisting) {
-            throw new RuntimeException("User with username " + user.getUserName() + " exists");
-        }
-
-        var role = roleRepository.findByRoleNameIgnoreCase(user.getUserRole())
-                .orElseGet(() -> new Role(null, user.getUserRole()));
-
-        user.setRoles(Set.of(role));
-        user.setUserPassword(passwordEncoder.encode(user.getUserPassword()));
-//        user.setUserAge(user.userGetAge());    * enable after datepicker will be placed in registration_form
-        user.setUserCreatedAt(LocalDate.now());
-        return userRepository.save(user);
-    }
-
+//    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+//        return roles.stream()
+//                .map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+//                .collect(toList());
+//    }
+//
+//    public User registerUser(User user) {
+//        boolean isUserExisting = userRepository.existsByUsername(user.getUserName());
+//        if (isUserExisting) {
+//            throw new RuntimeException("User with username " + user.getUserName() + " exists");
+//        }
+//
+//        var role = roleRepository.findByRoleNameIgnoreCase(user.getUserRole())
+//                .orElseGet(() -> new Role(null, user.getUserRole()));
+//
+//        user.setRoles(Set.of(role));
+//        user.setUserPassword(passwordEncoder.encode(user.getUserPassword()));
+////        user.setUserAge(user.userGetAge());    * enable after datepicker will be placed in registration_form
+//        user.setUserCreatedAt(LocalDate.now());
+//        return userRepository.save(user);
+//    }
 
 }
--- src/main/java/com/example/store/user/MyUserDetails.java
+++ src/main/java/com/example/store/user/MyUserDetails.java
@@ -16,6 +16,10 @@ public class MyUserDetails implements UserDetails {
 
     private User user;
 
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Set<Role> roles = user.getRoles();
