fa20d163b8c6232832a3063a166df62d98a6eff0
Optimus2525 <ilmrsm@gmail.com>
2021-09-29 20:20:48 +0300

    Added product images upload functionality

--- src/main/java/com/example/store/admin/ImageUploadController.java
+++ src/main/java/com/example/store/admin/ImageUploadController.java
@@ -1,40 +1,74 @@
 package com.example.store.admin;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Controller
-@Slf4j
-@RequiredArgsConstructor
 @RequestMapping("/admin")
 public class ImageUploadController {
 
-    private final ImageUploadService imageUploadService;
+    private final String UPLOAD_DIR = "src/main/resources/static/images/product_images/";
 
-    @GetMapping("/add_image")
-    public String addImage() {
-        return "/admin/upload_image";
+    @GetMapping("/upload_image")
+    public String homepage() {
+        return "admin/upload_image";
     }
 
-    @PostMapping("/upload_image")
-    public String upload_image(@RequestParam("imageFile") MultipartFile imageFile) {
-        String returnValue = "/admin/upload_image";
-        try {
-            imageUploadService.saveImage(imageFile);
-        } catch (Exception e) {
-            e.printStackTrace();
-            log.error("Error saving image", e);
-            returnValue = "error";
+    private static final String productNo = "555";
+    private static final String categoryNo = "222";
+    private static final String imageSize = "400";
+
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("image_file") MultipartFile file,
+                             HttpServletRequest request,
+                             RedirectAttributes attributes) throws ServletException, IOException {
+
+        // Check if file is empty
+        if (file.isEmpty()) {
+            attributes.addFlashAttribute("message", "Please select a file to upload.");
+            return "redirect:/admin/upload_image";
         }
-        return "redirect:/admin/add_image";
-    }
 
+        // Normalizing the file path and renaming the uploaded file
+        // FileName = filePart.getSubmittedFileName();
+        Part filePart = request.getPart("image_file");
+        // Full name of file submitted
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        // Split between name and extension
+        String[] fileNameSplit = fileName.split("\\.");
+        // Get the extension part of the file name
+        String fileExtension = fileNameSplit[fileNameSplit.length - 1];
+        // Combine all filename parts together
+        fileName = productNo + categoryNo + imageSize + "." + fileExtension;
+
+        // Save the file on the local file system
+        try {
+            Path path = Paths.get(UPLOAD_DIR + fileName);
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Return success response
+        attributes.addFlashAttribute("message", "Image <b>" + fileName + "</b> successfully uploaded to directory " + UPLOAD_DIR + "!");
+
+        return "redirect:/admin/upload_image";
+    }
 
 }
