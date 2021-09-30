0156b4e36660ca70005a844abd071224a2491b6d
Optimus2525 <ilmrsm@gmail.com>
2021-09-30 16:21:06 +0300

    Added in Admin Panel - View All Product Categories table

--- src/main/java/com/example/store/admin/AdminController.java
+++ src/main/java/com/example/store/admin/AdminController.java
@@ -2,6 +2,7 @@ package com.example.store.admin;
 
 import com.example.store.product.Product;
 import com.example.store.product.ProductService;
+import com.example.store.product_category.ProductCategory;
 import com.example.store.product_category.ProductCategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -27,13 +28,14 @@ public class AdminController {
         return "/admin/administration";
     }
 
+
     // Show all products existing in DB paginated
+    // ********************************************
     @GetMapping("/all_products")
     public String getAllProducts(Model model) {
-        model.addAttribute("all_product_categories", productCategoryService.findAlProductCategories());
+        model.addAttribute("all_product_categories", productService.findAllProducts());
         return getAllProductsByPage(model, 1, "productName", "asc");
     }
-
 
     @GetMapping("/all_products/page/{pageNumber}")
     public String getAllProductsByPage(
@@ -54,6 +56,36 @@ public class AdminController {
         String reverseSortDirection = sortDirection.equals("asc") ? "asc" : "desc";
         model.addAttribute("reverseSortDirection", reverseSortDirection);
         return "admin/list_all_products";
+    }
+
+
+    // Show all categories existing in DB paginated
+    // ********************************************
+    @GetMapping("/all_categories")
+    public String getAllCategories(Model model) {
+        model.addAttribute("all_product_categories", productCategoryService.findAlProductCategories());
+        return getAllCategoriesByPage(model, 1, "categoryName", "asc");
+    }
+
+    @GetMapping("/all_categories/page/{pageNumber}")
+    public String getAllCategoriesByPage(
+            Model model,
+            @PathVariable("pageNumber") int currentPage,
+            @Param("sortField") String sortField,
+            @Param("sortDirection") String sortDirection) {
+        Page<ProductCategory> page = productCategoryService.findAllPageable(currentPage, sortField, sortDirection);
+        long totalItems = page.getTotalElements();
+        int totalPages = page.getTotalPages();
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("totalItems", totalItems);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("all_categories", page.getContent());
+        model.addAttribute("paginationLink", "/products/all_categories/page/");
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDirection", sortDirection);
+        String reverseSortDirection = sortDirection.equals("asc") ? "asc" : "desc";
+        model.addAttribute("reverseSortDirection", reverseSortDirection);
+        return "/admin/list_all_categories";
     }
 
 
--- src/main/java/com/example/store/product_category/ProductCategoryService.java
+++ src/main/java/com/example/store/product_category/ProductCategoryService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -29,9 +30,10 @@ public class ProductCategoryService {
         productCategoryRepository.deleteById(id);
     }
 
-    // Find all products paginated
     public Page<ProductCategory> findAllPageable(int pageNumber, String sortField, String sortDirection) {
-        Pageable pageable = PageRequest.of(pageNumber - 1, 16);
+        Sort sort = Sort.by("categoryName");
+        sort = sortDirection.equals("asc") ? sort.ascending() : sort.descending();
+        Pageable pageable = PageRequest.of(pageNumber - 1, 20, sort);
         return productCategoryRepository.findAll(pageable);
     }
 
